d86cac056c72ef452b25333fa61b6da917be56fc
Daniel <mufy@abv.bg>
2017-02-17 21:58:31 +0200

    Array

---
+++ HomeWork/ConsoleApplication133/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication133
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            int[] array = new int [n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int result = array.Max();
+            Console.WriteLine(result);
+
+        }
+    }
+}
