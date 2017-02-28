fd4dbfa6eb5fcaa4132cc0e894336323751ecd45
Daniel <mufy@abv.bg>
2017-02-28 12:37:06 +0200

    Arrays
    HomeWork SoftUni

---
+++ Array and List Algorithms - Exercises/06. Batteries/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace simpleArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> mAh = Console.ReadLine().Split().Select(double.Parse).ToList();
+            List<double> mili = Console.ReadLine().Split().Select(double.Parse).ToList();
+            int hours = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < mAh.Count; i++)
+            {
+                double used = mili[i] * hours;
+                double left = mAh[i] - used;
+                double procent = left / (mAh[i] / 100.0);
+
+                if (left >= 0)
+                {
+                    Console.WriteLine("Battery {0}: {1:f2} mAh ({2:f2})%", i + 1, left, procent);
+                }
+                else
+                {
+                    int dead = (int)(mAh[i] / mili[i]) + 1;
+                    Console.WriteLine("Battery {0}: dead (lasted {1} hours)", i + 1, dead);
+                }
+            }
+        }
+    }
+}
---
+++ Array and List Algorithms - More Exercises/ConsoleApplication1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> result = new List<int> { 1, 2, 12, 15, 16, 17 };
+            List<int> sequenceForInsertInResult = new List<int> { 13, 14, 15, 16, 17, 18, 19 };
+
+            int count = 0;
+            int nextNum = 0;
+
+            for (int l = 0; l < result.Count; l++)
+            {
+                if (sequenceForInsertInResult[0] <= result[l])
+                {
+                    nextNum = result[l];
+                    break;
+                }
+            }
+
+            for (int m = 0; m < sequenceForInsertInResult.Count; m++)
+            {
+                if (sequenceForInsertInResult[m] <= nextNum)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            sequenceForInsertInResult.RemoveRange(count, sequenceForInsertInResult.Count - count);
+
+            Console.WriteLine(string.Join(" ", sequenceForInsertInResult));
+        }
+    }
+}
